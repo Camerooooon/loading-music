76950f23e0e34174f0a00f67f23402fb8847bf30
Cameron Bannasch <cameron.bannasch@gmail.com>
2023-09-16 14:39:25 -0700

    Add ping command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
 use std::env;
 use dotenv::dotenv;
 
-use serenity::{prelude::GatewayIntents, Client};
+mod commands;
+
+use serenity::{prelude::{GatewayIntents, EventHandler, Context}, Client, async_trait, model::prelude::{Interaction, InteractionResponseType, command::Command}};
+use serenity::model::gateway::Ready;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+
+    async fn interaction_create(&self, context: Context, interaction: Interaction) {
+
+        if let Interaction::ApplicationCommand(command) = interaction {
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(err) = command
+                .create_interaction_response(&context.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", err);
+            }
+        }
+        
+    }
+
+    async fn ready(&self, context: Context, ready: Ready) {
+
+        println!("The bot is online! {}", ready.user.id);
+
+        Command::create_global_application_command(context.http, |command| {
+            commands::ping::register(command)
+        }).await.expect("Could not generate ping command");
+
+
+
+    }
+    
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,7 +57,7 @@ async fn main() {
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(&token, intents).await.expect("Failed to create client");
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Failed to create client");
 
     client.start().await.expect("Failed to start client");
 
