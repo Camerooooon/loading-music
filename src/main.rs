978bc25e5836c60c38a692eb5d2b53a03998fd3b
Cameron Bannasch <cameron.bannasch@gmail.com>
2023-09-16 14:11:54 -0700

    Working discord connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
+use dotenv::dotenv;
 
 use serenity::{prelude::GatewayIntents, Client};
 
 #[tokio::main]
 async fn main() {
-
+    dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN environment variable (fix this up)");
 
     let intents = GatewayIntents::GUILD_MESSAGES
